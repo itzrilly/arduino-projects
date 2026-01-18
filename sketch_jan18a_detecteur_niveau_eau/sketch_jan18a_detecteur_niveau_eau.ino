5d202e165020dad304944e94a2cbc69b4190fcf6
RILWANOU GARBA <rilwanougarbarilly@gmail.com>
2026-01-18 12:45:46 +0100

    Detecteur de niveau d'eau

---
+++ sketch_jan18a_detecteur_niveau_eau/sketch_jan18a_detecteur_niveau_eau.ino
@@ -0,0 +1,26 @@
+const int waterPin = A0;
+const int ledPin = 6;
+const int buzzerPin = 5;
+
+int threshold = 500;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(waterPin);
+  Serial.println(value);
+
+  if (value < threshold) {
+    digitalWrite(ledPin, HIGH);
+    tone(buzzerPin, 2000);
+  } else {
+    digitalWrite(ledPin, LOW);
+    noTone(buzzerPin);
+  }
+
+  delay(200);
+}
